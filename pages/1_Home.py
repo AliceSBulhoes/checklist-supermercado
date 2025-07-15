f7f958aa963355dbbbb7311cba2a840d9c240e84
Alice <alicesb2006@gmail.com>
2025-07-15 14:30:06 -0300

    Fazendo a página Home

--- pages/1_Home.py
+++ pages/1_Home.py
@@ -1,9 +1,33 @@
 import streamlit as st
 import pandas as pd
+from components.auth import verifica_login
 
-btn = st.button("Sair")
-if btn:
-    # Limpa o estado da sessão
-    st.session_state.clear()
-    # Redireciona para a página de login
-    st.switch_page("./app.py")
+def home() -> None:
+    """Função para exibir a página inicial após o login."""
+    st.title("Página Inicial")
+    st.write("Bem-vindo(a) ao sistema!")
+    
+    # Exibe o cargo do usuário logado
+    if 'cargo' in st.session_state:
+        st.write(f"Cargo: {st.session_state['cargo']}")
+    
+    # Botão para iniciar o checklist
+    btn_checklist()
+
+def btn_checklist() -> None:
+    """Função para exibir o botão de checklist."""
+    # Botão para iniciar o checklist
+    btn = st.button("Iniciar Checklist", on_click=lambda: st.switch_page("./pages/2_Checklist.py"))
+
+    # Se o botão for clicado, redireciona para a página de checklist
+    if btn:
+        st.switch_page("./pages/2_Checklist.py")
+    
+def main() -> None:
+    """Função principal para executar a página inicial."""
+    # Verifica se o usuário está logado
+    verifica_login()
+    home()
+
+if __name__ == "__main__":
+    main()
